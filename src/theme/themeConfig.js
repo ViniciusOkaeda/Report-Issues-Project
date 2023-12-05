dc01c91ba923e7bcc87eaf2e39a7b146c8fc170c
ViniciusOkaeda <viniciusokaeda@gmail.com>
2023-12-05 14:53:12 -0300

    update theme functions

--- src/theme/themeConfig.js
+++ src/theme/themeConfig.js
@@ -85,29 +85,33 @@ const ThemeConfig = () => {
                     menuFontColor: ''
                 }
 
-                localStorage.setItem("palette-menu-list", JSON.stringify(palette_menu_color_list))
+                localStorage.setItem("palette-menu-color-list", JSON.stringify(palette_menu_color_list))
+
+            }
+            if(localStorage.getItem('palette-menu-type-list') == null) {
+                var palette_menu_type_list = {
+                    menuWidth: '',
+                    menuHeight: '',
+                    menuMinHeight: '',
+                    menuDisplay: '',
+                    menuMargin: ''
+                }
+
+                localStorage.setItem("palette-menu-type-list", JSON.stringify(palette_menu_type_list))
 
             }
 
 
             var palette_theme_list = JSON.parse(localStorage.getItem('palette-theme-list'))
-
             setTheme(palette_theme_list);
 
 
-
-            var palette_menu_color_list = JSON.parse(localStorage.getItem('palette-menu-list'))
-                        
+            var palette_menu_color_list = JSON.parse(localStorage.getItem('palette-menu-color-list'))
             setMenuColor(palette_menu_color_list);
 
 
-
-            const currentMenuWidth = localStorage.getItem('menuWidth');
-            const currentMenuHeight = localStorage.getItem('menuHeight');
-            const currentMenuMinHeight = localStorage.getItem('menuMinHeight');
-            const currentMenuDisplay = localStorage.getItem('menuDisplay');
-            const currentMenuMargin = localStorage.getItem('menuMargin');
-            setMenuStyle(currentMenuWidth, currentMenuHeight, currentMenuMinHeight, currentMenuDisplay, currentMenuMargin);
+            var palette_menu_type_list = JSON.parse(localStorage.getItem('palette-menu-type-list'))
+            setMenuStyle(palette_menu_type_list);
 
     
 
@@ -158,50 +162,44 @@ const ThemeConfig = () => {
 
     }
 
-
-
-    const setMenuColor = (palette_menu_list) => {
-        document.documentElement.style.setProperty('--menu-color', palette_menu_list.menuColor);
-        document.documentElement.style.setProperty('--font-menu-color', palette_menu_list.menuFontColor);
+    const setMenuColor = (palette_menu_color_list) => {
+        document.documentElement.style.setProperty('--menu-color', palette_menu_color_list.menuColor);
+        document.documentElement.style.setProperty('--font-menu-color', palette_menu_color_list.menuFontColor);
     }
 
     const setPaletteMenu = (event) => {
-        var palette_menu_list = {
+        var palette_menu_color_list = {
             menuColor: event.target.style.getPropertyValue('--menu-color'),
             menuFontColor: event.target.style.getPropertyValue('--font-menu-color')
         }
 
 
-        setMenuColor(palette_menu_list);
+        setMenuColor(palette_menu_color_list);
 
-        localStorage.setItem("palette-menu-list", JSON.stringify(palette_menu_list))
+        localStorage.setItem("palette-menu-color-list", JSON.stringify(palette_menu_color_list))
 
 
     }
 
-    const setMenuStyle = (menuWidth, menuHeight, menuMinHeight, menuDisplay, menuMargin) => {
-        document.documentElement.style.setProperty('--menu-width', menuWidth);
-        document.documentElement.style.setProperty('--menu-height', menuHeight);
-        document.documentElement.style.setProperty('--menu-min-height', menuMinHeight);
-        document.documentElement.style.setProperty('--menu-display', menuDisplay);
-        document.documentElement.style.setProperty('--menu-margin', menuMargin);
+    const setMenuStyle = (palette_menu_type_list) => {
+        document.documentElement.style.setProperty('--menu-width', palette_menu_type_list.menuWidth);
+        document.documentElement.style.setProperty('--menu-height', palette_menu_type_list.menuHeight);
+        document.documentElement.style.setProperty('--menu-min-height', palette_menu_type_list.menuMinHeight);
+        document.documentElement.style.setProperty('--menu-display', palette_menu_type_list.menuDisplay);
+        document.documentElement.style.setProperty('--menu-margin', palette_menu_type_list.menuMargin);
     }
 
     const setMenuType = (event) => {
-        const currentMenuWidth = event.target.style.getPropertyValue('--menu-width');
-        const currentMenuHeight = event.target.style.getPropertyValue('--menu-height');
-        const currentMenuMinHeight = event.target.style.getPropertyValue('--menu-min-height');
-        const currentMenuDisplay = event.target.style.getPropertyValue('--menu-display');
-        const currentMenuMargin = event.target.style.getPropertyValue('--menu-margin');
+        var palette_menu_type_list = {
+            menuWidth: event.target.style.getPropertyValue('--menu-width'),
+            menuHeight: event.target.style.getPropertyValue('--menu-height'),
+            menuMinHeight: event.target.style.getPropertyValue('--menu-min-height'),
+            menuDisplay: event.target.style.getPropertyValue('--menu-display'),
+            menuMargin: event.target.style.getPropertyValue('--menu-margin')
+        }
 
-        setMenuStyle(currentMenuWidth, currentMenuHeight, currentMenuMinHeight, currentMenuDisplay, currentMenuMargin);
-
-        localStorage.setItem('menuWidth', currentMenuWidth);
-        localStorage.setItem('menuHeight', currentMenuHeight);
-        localStorage.setItem('menuMinHeight', currentMenuMinHeight);
-        localStorage.setItem('menuDisplay', currentMenuDisplay);
-        localStorage.setItem('menuMargin', currentMenuMargin);
-
+        setMenuStyle(palette_menu_type_list);
+        localStorage.setItem("palette-menu-type-list", JSON.stringify(palette_menu_type_list))
     }
 
 
